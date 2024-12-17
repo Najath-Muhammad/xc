86c50a80a2072faacbc71c6f314b18882226ddd3
Najath <najathmuhammad22@gmail.com>
2024-12-17 12:20:46 +0530

    schema completed

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const env = require('dotenv').config();
 const db = require('./config/db');
-db()
+const path = require('path');
+const userRouter = require('./routes/userRouter')
+db();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.set('view engine','ejs');
+app.set('views',[path.join(__dirname,'views/user'),path.join(__dirname,'view/admin')]);
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/',userRouter);
 
 app.listen(process.env.PORT,()=>{
     console.log('server started')
