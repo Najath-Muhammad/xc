073d7ad19e82f3c991b5a5d4ab5ae0a4c6a0e0ab
Najath <najathmuhammad22@gmail.com>
2024-12-20 17:23:16 +0530

    otp varification

--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -1,4 +1,8 @@
-const user = require('../../models/userSchema');
+const User = require('../../models/userSchema');
+const nodemailer = require('nodemailer');
+const env = require('dotenv').config();
+const bcrypt = require('bcrypt');
+
 
 
 
@@ -34,25 +38,114 @@ const loadSignup = async (req,res)=>{
     }
 }
 
+
+//otp generation
+function generateOtp(){
+    return Math.floor(100000 + Math.random()*900000).toString();
+}
+
+//OTP varification
+async function sendVerification(email,otp){
+    try {
+        const transporter = nodemailer.createTransport({
+            service:'gmail',
+            port:587,
+            secure:false,
+            requireTLS:true,
+            auth:{
+                user:process.env.NODEMAILER_EMAIL,
+                pass:process.env.NODEMAILER_PASSWORD
+            },
+        });
+        const info = await transporter.sendMail({
+            from:process.env.NODEMAILER_EMAIL,
+            to: email,
+            subject: 'Verify your email',
+            text:`Your OTP is ${otp}`,
+            html:`<b>Your OTP: ${otp}</b>`
+        })
+        return info.accepted.length > 0;
+    } catch (error) {
+        console.log("Error sending OTP",error);
+        return false;
+    }
+}
+
+  
+
+
 const signUp = async (req,res)=>{
     const {fullname,email,phone,password} = req.body;
-    console.log(req.body)
     
     try{
-       const newUser = new user({fullname,email,phone,password});
-       await newUser.save();
-       console.log(newUser)
+       const newUser = new User({fullname,email,phone,password});
+       const findUser = await User.findOne({email});
+       if(findUser){
+        return res.render('signup',{message:"email already exist"});
+       }
 
-       return res.redirect('/signup')
+       const otp = generateOtp();
+       const emailSent = await sendVerification(email,otp);
+       if(!emailSent){
+        return res.json("email error")
+       }
+       else{
+        req.session.userOtp = otp;
+        req.session.userData = {fullname,email,phone,password}
+ 
+        res.render('verify-otp');
+        console.log("OTP sent",otp);
+       }
+      
     }
     catch(error){
-        console.log('Error in Sign-Up',error)
-        res.status(500).send('internal server error')
+        console.log('Error in Sign-Up',error);
+        res.redirect("/pageNotFound");
+    }
+}
+
+
+const securePassword = async (password)=>{
+  try {
+    const passworHash = await bcrypt.hash(password,10)
+    return passworHash;
+  } catch (error) {
+    console.log('Error hashing password:', error);
+    throw new Error('Error hashing password');
+  }
+}
+
+const varifyOtp =async(req,res)=>{
+    try{
+        const {otp} = req.body;
+        console.log(otp)
+        if(otp === req.session.userOtp){
+            const user = req.session.userData;
+            const passwordHash = await securePassword(user.password);
+
+            const saveUserData = new User({
+               fullname:user.fullname,
+               email:user.email,
+               phone:user.phone,
+               password:passwordHash
+            })
+
+            await saveUserData.save();
+            req.session.user = saveUserData._id;
+            res.json({success:true,redirectUrl:'/'})  //path after the signUp
+        }
+        else{
+            res.status(400).json({success:false,message:'invalid OTP'})
+        }
+    }
+    catch(error){
+        console.error('Error Verifying OTP',error);
+        res.status(500).json({success:false,message:'Internal Server error'})
     }
 }
 
 
 
 module.exports = {
-    loadHomepage,pageNotFound,loadSignup,signUp
+    loadHomepage,pageNotFound,loadSignup,signUp,varifyOtp
 }
